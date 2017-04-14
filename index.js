bdf17585a509c03259f4e25d1610a31d6683c26a
Alan Fidelino <afidedev26@gmail.com>
2017-04-14 20:02:29 +1000

    Add default route

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ var port = process.env.port || 3000;
 
 app.use(express.static(__dirname+'/public'));
 
+
+app.get('/', (req,res)=>{
+  res.render('./public/index.html')
+})
 app.listen(port, ()=>{
   console.log(`Server running at port:${port}`);
 })
