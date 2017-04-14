1103a06ff3f49125be24033ef1d8f01d65778fc8
Alan Fidelino <afidedev26@gmail.com>
2017-04-14 20:14:36 +1000

    Amend index.js to serve public files

--- index.js
+++ index.js
@@ -6,9 +6,6 @@ var port = process.env.port || 3000;
 app.use(express.static(__dirname+'/public'));
 
 
-app.get('/', (req,res)=>{
-  res.render('./public/index.html')
-})
 app.listen(process.env.port || 3000, ()=>{
   console.log(`Server running at port:3000`);
 })
