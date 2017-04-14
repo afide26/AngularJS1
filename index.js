ef038b5cbe112cf89c0f7177e8c0df64cbe7ec9f
Alan Fidelino <afidedev26@gmail.com>
2017-04-14 20:18:26 +1000

    Fix typo of port to PORT

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 var express = require('express');
 var app = express();
 
-var port = process.env.port || 3000;
+var port = process.env.PORT || 3000;
 
 app.use(express.static(__dirname+'/public'));
 
 
-app.listen(process.env.port || 3000, ()=>{
+app.listen(port, ()=>{
   console.log(`Server running at port:3000`);
 })
